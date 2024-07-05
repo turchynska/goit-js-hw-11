ba5e68944a738d72eed1e28c97ad83d3e8b910fe
Kate <159571202+turchynska@users.noreply.github.com>
2024-07-05 17:08:41 -0400

    hm11

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import { renderImage } from "./js/render-functions";
 const btn = document.querySelector(".search-btn");
 const form = document.querySelector(".search-form");
 const input = document.querySelector(".search-input");
-const gallery = document.querySelector(".gallery");
+const list = document.querySelector(".gallery");
 const loader = document.querySelector(".loader")
 
 form.addEventListener("submit", (event) =>{
@@ -32,7 +32,32 @@ fetchImages(value)
 .then(data => {
     if( data.hits && data.hits.length > 0){
         const markup = renderImage(data.hits);
+        list.insertAdjacentHTML("beforeend", markup);
+        loader.classList.toggle("visual-hidden");
+
+
+        let lightbox = new SimpleLightbox (".gallery a", {
+            captions: true,
+            captionDelay: 250,
+            captionPosition: 'bottom',
+            captionsData: 'alt',
+        });
+
+lightbox.refresh();
+    } else{
+        iziToast.error({
+            position: "topRight",
+            message: 'Sorry, there are no images matching your search query. Please try again!',
+        })
+        loader.classList.toggle("visually-hidden");
     }
 })
-}
-)
+.catch(err =>{
+    console.log(err);
+    iziToast.error({
+        position: "topRight",
+        message: "Sorry, the request cant be completed at this time. Please try again"
+    })
+});
+input.value = "";
+});
